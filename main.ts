1be40aaf776288307cb2f88c366e87972c0c0476
1711011 <104405451+1711011@users.noreply.github.com>
2022-06-02 10:10:30 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+music.setVolume(255)
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(330, music.beat(BeatFraction.Half))
 })
